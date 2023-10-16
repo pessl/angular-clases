a1b916e703be101979afb0dcb5c19839af7e201d
vtejada <vgtejada@gmail.com>
2023-10-15 20:26:20 -0500

    agregando ruteo de acuerdo al tipo de acompañamiento

--- src/app/views/forms/correo-respuesta-asa/correo-respuesta-asa.component.ts
+++ src/app/views/forms/correo-respuesta-asa/correo-respuesta-asa.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ContextoSolicitudASAService, TipoAcompanamiento } from 'src/app/services/ASA/contexto-solicitud-asa.service';
 
 @Component({
   selector: 'app-correo-respuesta-asa',
@@ -7,5 +9,33 @@ import { Component } from '@angular/core';
   ]
 })
 export class CorreoRespuestaAsaComponent {
+
+  constructor(
+    private router: Router,
+    private contexto: ContextoSolicitudASAService) {
+  }
+
+  irASiguiente() {
+    if (this.contexto.aceptaSolicitud) {
+      const tipoAcompanamiento = this.contexto.tipoAcompanamiento;
+      if (tipoAcompanamiento) {
+        switch (tipoAcompanamiento) {
+          case TipoAcompanamiento.EAT:
+            this.router.navigate(['/forms/documentos-eat']);
+            break;
+          case TipoAcompanamiento.EAC:
+            this.router.navigate(['/forms/documentos-eac']);
+            break;
+          case TipoAcompanamiento.SOPORTE_PARTICIPACION:
+            this.router.navigate(['/forms/reporte-participacion-asa']);
+            break;
+        }
+      } else {
+        // ERROR: el tipo de acompañamiento tuvo que ser seteado anteriormente
+      }
+    } else {
+      this.router.navigate(['/forms']);
+    }
+  }
 
 }
