fa4a2e036dd3752026648aa8f1a99de94fcb67e9
vtejada <vgtejada@gmail.com>
2023-10-15 19:46:32 -0500

    agregando el servicio context-solicitud-asa.service que nos va a ayudar a guardar el estado del tipo de acompañamiento

--- src/app/views/forms/registrar-solicitud-asa/registrar-solicitud-asa.component.ts
+++ src/app/views/forms/registrar-solicitud-asa/registrar-solicitud-asa.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { BuscarAdministradoComponent } from '../buscar-administrado/buscar-administrado.component';
 import { Administrado } from 'src/app/interface/ASA/administrado';
+import { ContextoSolicitudASAService } from 'src/app/services/ASA/contexto-solicitud-asa.service';
 
 @Component({
   selector: 'app-registrar-solicitud-asa',
@@ -12,7 +13,9 @@ export class RegistrarSolicitudAsaComponent {
 
   listaDeAdministrados: Administrado[] = [];
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private contexto: ContextoSolicitudASAService) {
+    this.contexto.limpiar();
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(BuscarAdministradoComponent);
